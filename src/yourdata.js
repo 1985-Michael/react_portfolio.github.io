42acc6d63c159f3e9a1ac0efa137e2ce2f3744f2
Michael Myers <anthonymyers1985@gmail.com>
2020-05-26 20:33:50 -0700

    created template and components

---
+++ src/yourdata.js
@@ -0,0 +1,34 @@
+export default {
+  headerTagline: ['I am Michael Myers.', ' Welcome to my Portfolio '],
+  contactEmail: 'anthonymyers1985@gmail.com',
+
+  abouttext: 'Artist / Graphic Designer / Web Developer',
+  aboutImage: 'https://i.ibb.co/QDSRpn1/mikey.jpg',
+
+  ShowAboutImage: true,
+
+  projects: [
+    {
+      id: 1,
+      title: 'Food Radar',
+      imageSrc: 'https://i.ibb.co/RzHw1yB/four.png0',
+      url: 'https://vartanyane.github.io/project-1/',
+    },
+
+    {
+      id: 2,
+      title: 'Would You Rather',
+      imageSrc: 'https://i.ibb.co/GkJP1JK/five.png',
+      url: 'https://would--you--rather.herokuapp.com/',
+    },
+
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: 'Code Quiz',
+      imageSrc: 'https://i.ibb.co/RSBFXvY/three.png',
+      url: 'https://1985-michael.github.io/_Mike_CodeQuiz.github.io/',
+    },
+  ],
+
+  social: [{ name: 'Github', url: 'https://github.com/1985-Michael' }],
+};
